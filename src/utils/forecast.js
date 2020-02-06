579dd7f738482c5a3a73af72e4ab8962c66d4b41
Shreyas <shreyas.kamarthi@gmail.com>
2020-02-07 01:05:18 +0530

    changes to weather to SI unit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/64f970472b9bafec458326801dee6b58/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/64f970472b9bafec458326801dee6b58/' + latitude + ',' + longitude + '?units=si'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,6 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
+            console.log(body.daily.data[0]);
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
